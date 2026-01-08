e0876f6255052801945dff84018b1cac0a5067bd
BBogdan23 <bogdan-laurian.beleut@student.upt.ro>
2026-01-08 18:48:05 +0200

    client1.c

--- client1.c
+++ client1.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*
+ Client 1 - client embedded (simulat)
+ Rol: detectare miscare si publicare evenimente
+*/
 
 int main() {
-    printf("Client 1 - publicare evenimente de miscare (simulat)\n");
+    int motion_detected;
+    srand(time(NULL));
+
+    printf("Client 1 pornit - simulare senzor PIR\n");
+
+    // simulare evenimente de miscare
+    for (int i = 0; i < 5; i++) {
+        motion_detected = rand() % 2;
+
+        if (motion_detected) {
+            printf("Eveniment: miscare detectata -> publicare mesaj MQTT\n");
+        } else {
+            printf("Eveniment: fara miscare\n");
+        }
+    }
+
     return 0;
 }
