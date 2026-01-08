12cec1dad5cd7a1eb6ccd2ffed6eb41096fd858e
BBogdan23 <bogdan-laurian.beleut@student.upt.ro>
2026-01-08 18:50:16 +0200

    client2.c

--- client2.c
+++ client2.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
+/*
+ Client 2 - client receptor (simulat)
+ Rol: receptie si procesare date pentru control iluminat
+*/
+
 int main() {
-    printf("Client 2 - receptie si procesare date (simulat)\n");
+    printf("Client 2 pornit - abonare la topic MQTT\n");
+
+    printf("Mesaj receptionat: miscare detectata\n");
+    printf("Actiune: iluminat PORNIT\n");
+
+    printf("Mesaj receptionat: fara miscare\n");
+    printf("Actiune: iluminat OPRIT\n");
+
     return 0;
 }
